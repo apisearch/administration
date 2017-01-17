5b9dbc8db7d63e700a5fabb1e2d4e3f2c4205dc4
Luděk Veselý <ludek.vesely@email.com>
2017-01-18 00:43:35 +0100

    init app skeleton

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# administration
-GUI for apisearch
+# GUI for apisearch
+
+### Usage
+
+- run [apisearch/apisearch](https://github.com/apisearch/apisearch) project
+- `composer install`
+- `chmod -R 777 temp log`
+- `php -S localhost:8000 -t www`
+- `open http://localhost:8000`
---
+++ app/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+$configurator->setTimeZone('Europe/Prague');
+$configurator->enableTracy(__DIR__ . '/../log');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->register();
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+
+$container = $configurator->createContainer();
+
+return $container;

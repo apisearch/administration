b17da13d9ddb960502416e453b2240937afe2c36
Luděk Veselý <ludek.vesely@email.com>
2017-01-20 00:45:02 +0100

    :lipstick: rearrange templates layout

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,4 +5,16 @@ namespace App\Presenters;
 
 class HomepagePresenter extends BasePresenter
 {
+    /** @var string @persistent */
+    public $token;
+
+    public function startup()
+    {
+        if (!$this->token) {
+            $this->flashMessage('Nejste přihlášeni!', 'danger');
+            $this->redirect('Sign:');
+        }
+
+        parent::startup();
+    }
 }
